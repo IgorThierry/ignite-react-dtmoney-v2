81093df2945679e50626749c0b521986511dccb1
Igor Thierry <igorthierry15@gmail.com>
2023-10-04 15:35:56 -0300

    feat: set axios baseURL if not in .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,10 @@ import axios from 'axios'
 
 // axios.defaults.withCredentials = true
 
+const apiURL = import.meta.env.VITE_API_URL
+const baseURL = apiURL ? String(apiURL) : 'http://localhost'
+
 export const api = axios.create({
-  baseURL: String(import.meta.env.VITE_API_URL),
+  baseURL,
   withCredentials: true,
 })
