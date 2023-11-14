5dc8a3cb8c325bafaa4dbcef254e3fa6ba28ea48
Igor Thierry <igorthierry15@gmail.com>
2023-11-14 13:31:36 -0300

    update globalloading

--- src/components/GlobalLoading/index.tsx
+++ src/components/GlobalLoading/index.tsx
@@ -1,6 +1,7 @@
 import { useContextSelector } from 'use-context-selector'
 import { GlobalLoadingContext } from '../../contexts/GlobalLoading'
-import { Message, Overlay, Spinner } from './styles'
+
+import { Flex, Spinner, Text } from '@chakra-ui/react'
 
 export function GlobalLoading() {
   const { show, msg } = useContextSelector(GlobalLoadingContext, (context) => {
@@ -15,9 +16,36 @@ export function GlobalLoading() {
   }
 
   return (
-    <Overlay>
-      <Spinner />
-      {msg && <Message>{msg}</Message>}
-    </Overlay>
+    <Flex
+      direction="column"
+      pos="fixed"
+      top="0"
+      left="0"
+      w="100%"
+      h="100%"
+      bg="rgba(255, 255, 255, 0.8)"
+      justify="center"
+      align="center"
+      zIndex="9999"
+    >
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+      {msg && (
+        <Text
+          textTransform="capitalize"
+          marginTop="16px"
+          fontSize="1.5rem"
+          fontWeight="bold"
+          color="blue.500"
+        >
+          {msg}
+        </Text>
+      )}
+    </Flex>
   )
 }
